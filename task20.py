6b0efa321de8c8827058a4cbc6db0412a60210da
malewin <139165145+malewin@users.noreply.github.com>
2023-10-23 23:03:58 +0900

    languageDetected method is added

--- task20.py
+++ task20.py
@@ -22,27 +22,31 @@
 12
 """
 
-
-slovo = input('enter your word: ').lower()
+slovo = input('enter your word / введите ваше слово: ').lower()
 engAlphabet = {'a':1, 'e':1, 'i':1, 'o':1, 'u':1, 'l':1, 'n':1, 's':1, 't':1, 'r' :1, 'd':2, 'g':2, 'b':3, 'c':3, 'm':3, 'p':3, 'f':4, 'h':4, 'v':4, 'w':4, 'y':4, 'k':5, 'j':8, 'x':8, 'q':10, 'z':10}
 rusAlphabet = {'й':4, 'ц':5, 'у':2, 'к':2, 'е':1, 'н':1, 'г':3, 'ш':8, 'щ':10, 'з':5, 'х':5, 'ъ':10, 'ф':10, 'ы':4, 'в':1, 'а':1, 'п':2, 'р':1, 'о':1, 'л':2, 'д':2, 'ж':5, 'э':8, 'ё':3, 'я':3, 'ч':5, 'с':1, 'м':2, 'и':1, 'т':1, 'ь':3, 'б':3, 'ю':8}
 
+def languageDetected(dict1, dict2, word): # метод распознавания языка для декомпозиции
+    dictionary = {}
+    if bool(set(word).intersection(set(dict1))) == True:
+        dictionary = dict1
+    else:
+        dictionary = dict2
+    return dictionary
 
-def scoresCounter(dict1, dict2, word):
+def scoresCounter(dict, word): # метод подсчёта очков
     counter = 0
     for i in word:
-        for j,k in dict1.items():
+        for j,k in dict.items():
             if i==j:
                 counter += k
-            
-        for j,k in dict2.items():
-            if i==j:
-                counter += k
-
     return counter
 
-result = scoresCounter(engAlphabet,rusAlphabet, slovo)
+result = scoresCounter(languageDetected(engAlphabet, rusAlphabet, slovo), slovo)
 
 print(result)
 
 # print(list('йцукенгшщзхъфывапролджэёячсмитьбю'))
+
+# print(bool(set(engAlphabet).intersection(set(slovo))))
+
