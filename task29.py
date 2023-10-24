ee26c9cef14fbfd84b626b2fcc5b8693c1e2ff33
malewin <139165145+malewin@users.noreply.github.com>
2023-10-24 23:11:34 +0900

    solution is right

--- task29.py
+++ task29.py
@@ -2,23 +2,6 @@
 # Ваня и Петя поспорили, кто быстрее решит следующую задачу: “Задана последовательность неотрицательных целых чисел. Требуется определить значение наибольшего элемента последовательности, которая завершается первым встретившимся нулем (число 0 не входит в последовательность)”. Однако 2 друга оказались не такими смышлеными. Никто из ребят не смог до конца сделать это задание. Они решили так: у кого будет меньше ошибок в коде, тот и выиграл спор. За помощью товарищи обратились к Вам, студентам.
 # Примечание: Программные коды на следующих слайдах
 # 30 минут
-# Ваня:
-# n = int(input())
-#  max_number = 1000
-#  while n != 0:
-#  n = int(input())
-#  if max_number > n:
-#  max_number = n
-#  print(max_number)        
-# Петя:
-#  n = int(input())
-#  max_number = -1
-#  while n < 0:
-#  n = int(input())
-#  if max_number < n:
-#  n = max_number
-# 30 минут
-#  print(n)
   
 from random import randint # необходим модуль для функции рандомного заполнения
 n = int(input('сколько чисел в вашем списке?: ')) # количество элементов в рандомном списке
@@ -36,9 +19,12 @@ print(f'Ваш изначальный список: {listOfDiffValue}')
 def listSeparator(userList):
     maxValue = userList[0]
     for i in userList:
-        while i != 0:
-            if maxValue < i:
-                maxValue = i
+        if maxValue < i:
+            maxValue = i
+        else:
+            continue
+        if i == 0:
+            break       
     return maxValue
 
 print(listSeparator(listOfDiffValue))
