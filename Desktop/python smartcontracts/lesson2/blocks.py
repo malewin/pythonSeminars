c032600a73eb78bd8e65d7cbe1ef2b685e7e563d
malewin <139165145+malewin@users.noreply.github.com>
2023-11-04 22:57:46 +0900

    lesson2 is finished

--- Desktop/python smartcontracts/lesson2/blocks.py
+++ Desktop/python smartcontracts/lesson2/blocks.py
@@ -21,13 +21,31 @@ async def get_client(index):
 async def get_block_transaction(client: TonlibClient, wc, shard, seqno):
     return await client.get_block_transactions(workchain=wc, shard=shard, seqno=seqno, count=40)['transactions']  # count - количество запрашиваемых транзакций (по стоку идет 40)
 
+# async def main():
+#     client = await get_client(11)
+
+#     last_master_block = (await client.get_masterchain_info())['last']
+
+#     print(await get_block_transaction(client, -1, last_master_block['shard'], last_master_block['seqno']))
 
 async def main():
-    client = await get_client(0)
+    client = await get_client(11)
 
     last_master_block = (await client.get_masterchain_info())['last']
 
-    print(await get_block_transaction(client, -1, last_master_block['shard'], last_master_block['seqno']))
+    # print(last_master_block)
+
+    base_data = (await client.get_shards(master_seqno=last_master_block['seqno']))['shards'][0]     # workchain=0 - значит basechain, ещё есть masterchain
+    
+    base_shard = base_data['shard']
+    base_seqno = base_data['seqno']
+
+    transactions = await get_block_transaction(client, 0, base_shard, base_seqno)
+
+    for tr in transactions:
+        full_tr = (await client.get_transactions(account=tr['account'], from_transaction_lt=tr['lt'], from_transaction_hash=tr['hash']))[0]
+        print(full_tr)
+
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
