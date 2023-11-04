8257b0c623fd35a8e5e7901447df406dd7df061f
malewin <139165145+malewin@users.noreply.github.com>
2023-11-04 17:40:04 +0900

    SliceMethod is described

--- Desktop/python smartcontracts/lesson2/cells.py
+++ Desktop/python smartcontracts/lesson2/cells.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     # print(x.to01())
 
     op_code = int(x[:32].to01(), 2)
-    print(op_code)
+    # print(op_code)
     del x[:32] # удаляем ненужные биты информации из массива
 
     # print(bin(int(array.hex(), 16)))  # без 28 нулей
@@ -39,5 +39,28 @@ if __name__ == '__main__':
 
     # print(address)
     print(Address(address).to_string(True, True, True)) # to_string(true, true, true) - перерводит в userFriendlyBounceableForm
+
+    del x[:256]
+
+    l = int(x[:4].to01(), 2)
+
+    del x[:4]
+
+    amount = int(x[:l * 8].to01(), 2)
+    print(amount)
+
+# далее способ парсинга ячейки с помощью библиотеки поддерживающей "СЛАЙСЫ"
+# нужно удалить библиотеку tonsdk и поставить её форк от DAO чтото там..
+# from tonsdk.boc import Slice - импортировать метод слайсов и далее код/
+
+# def tonsdk_parsing():
+#     slice = Slice(message) # применили метод к нашему сообщению с ячейкой и присвоили в перменную
+
+#     op_code = slice.read_uint(32)
+#     address = slice.read_msg_addr().to_string(True, True, True)
+#     coins = slice.read_coins()
+
+#     print(op_code, address, coins)
+
 
 
