3a073706662c7fcbb488b55d3a56d1cbeb3ae0d9
malewin <139165145+malewin@users.noreply.github.com>
2023-11-05 16:45:55 +0900

    fulled for mnemonics

---
+++ Desktop/python smartcontracts/lesson3/secret.py
@@ -0,0 +1,17 @@
+from tonsdk.contract.wallet import Wallets, WalletVersionEnum
+
+mnemonics = ['suspect', 'indicate', 'absorb', 'educate', 
+            'share', 'fossil', 'foam', 'good', 'glare', 
+            'predict', 'stem', 'depart', 'domain', 'east', 
+            'benefit', 'fragile', 'jelly', 'loyal', 'toilet', 
+            'merry', 'pride', 'increase', 'marble', 'erode']
+mnemonics, pub_k, priv_k, wallet = Wallets.from_mnemonics(mnemonics = mnemonics, version = WalletVersionEnum.v3r2, workchain = 0) # wallets.create()- чтоб создать
+
+wallet_address = (wallet.address.to_string(True, True, True, True))
+
+if __name__ == '__main__':
+    print(mnemonics)
+    # print(wallet.address.to_string()) # hex - format (16ричное число)
+    # print(wallet.address.to_string(True, True, True)) # привычная форма кошелька
+    # wallet_address = (wallet.address.to_string(True, True, True, True)) # test.net - формат
+    print(wallet_address)
