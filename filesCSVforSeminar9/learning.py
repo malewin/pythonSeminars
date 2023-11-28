68a74bb085316a31639400e35637a0491d2bda3c
malewin <139165145+malewin@users.noreply.github.com>
2023-11-29 08:02:44 +0900

    fixing a files green color

---
+++ filesCSVforSeminar9/learning.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('california_housting_test.csv')
+
+df.head(10)
---
+++ test.py
@@ -0,0 +1,13 @@
+clients = ['tondomainer', 'author', 'elector', 'fuhao']
+domainsValue = [1140, 381, 150, 20]
+paidTonAmount = ['10500ton', '9700ton', '15000ton', '20000ton']
+slovar =list(zip(clients, domainsValue, paidTonAmount))
+# print(slovar)
+print(list(enumerate(slovar)))
+
+
+# clients = ['tondomainer', 'author', 'elector', 'fuhao']
+# print(dict(enumerate(clients)))
+# print(tuple(enumerate(clients)))
+# print(set(enumerate(clients)))
+# print(list(enumerate(clients)))
